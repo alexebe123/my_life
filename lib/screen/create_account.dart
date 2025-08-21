a2eaf8f2eecd84c14b42d249bbed3c1ec7840b15
alexebe123 <alaabassi14@gmail.com>
2025-08-21 21:00:31 +0100

    update photo and show in screen

--- lib/screen/create_account.dart
+++ lib/screen/create_account.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sizer/sizer.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:flutter/services.dart';
+import 'package:my_life/model/uplode_model.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
@@ -10,6 +17,37 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  List<FileUploadModel?> listImages = [];
+  Future<FileUploadModel?> pickImage(ImageSource source) async {
+    FileUploadModel? finalResult;
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) {
+        return null;
+      }
+
+      finalResult = FileUploadModel(file: File(image.path), name: image.name);
+
+      try {
+        final compresedResult = await FlutterImageCompress.compressWithFile(
+          image.path,
+          minWidth: 720,
+          minHeight: 540,
+          quality: 80,
+        );
+        if (compresedResult != null) {
+          finalResult.file.writeAsBytesSync(compresedResult);
+        }
+      } catch (e) {
+        print("amidi error $e");
+      }
+      return finalResult;
+    } on PlatformException catch (e) {
+      print("failed to pick image: $e");
+    }
+    return finalResult;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,10 +98,8 @@ class _CreateAccountState extends State<CreateAccount> {
               ),
             ),
             SizedBox(height: 3.h),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                ClipRRect(
+            listImages.isNotEmpty
+                ? ClipRRect(
                   borderRadius: BorderRadius.all(Radius.circular(100)),
                   child: Container(
                     width: 50.w,
@@ -71,14 +107,49 @@ class _CreateAccountState extends State<CreateAccount> {
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(30)),
                     ),
-                    child: Container(
-                      color: const Color.fromARGB(255, 233, 247, 233),
+                    child: CircleAvatar(
+                      maxRadius: 15.h,
+                      child: Image.file(
+                        listImages[0]!.file,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
+                )
+                : Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () async {
+                        // Handle photo upload
+                        // Handle photo upload
+                        final result = await pickImage(ImageSource.gallery);
+                        if (result == null) {
+                          return;
+                        }
+                        setState(() {
+                          listImages.add(result);
+                        });
+                        //Provider.of<ApiServiceFirebase>(context, listen: false).uploadPhoto();
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(100)),
+                        child: Container(
+                          width: 50.w,
+                          height: 20.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                          child: Container(
+                            color: const Color.fromARGB(255, 233, 247, 233),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text('Add Photo'),
+                  ],
                 ),
-                Text('Add Photo'),
-              ],
-            ),
             SizedBox(height: 2.h),
             Container(
               alignment: Alignment.topLeft,
